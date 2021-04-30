1bc6aaf4cc3d9aea966db4ea7681e009d287e3dc
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-04-30 23:17:16 +0900

    Initial commit for blog backend

---
+++ src/api/posts/posts.ctrl.js
@@ -0,0 +1,11 @@
+import Post from '../../models/post';
+
+export const write = (ctx) => {};
+
+export const list = (ctx) => {};
+
+export const read = (ctx) => {};
+
+export const remove = (ctx) => {};
+
+export const update = (ctx) => {};
