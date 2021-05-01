33fbc6134a27b79c8a542ba0e43827f3677ebc9b
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-05-01 16:05:14 +0900

    Add jwt sign method in user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const UserSchema = new Schema({
   username: String,
@@ -20,6 +21,26 @@ UserSchema.methods.checkPassword = async function (password) {
 UserSchema.statics.findByUsername = function (username) {
   //this는 모델을 가리킴
   return this.findOne({ username });
+};
+
+UserSchema.methods.serialize = function () {
+  const data = this.toJSON();
+  delete data.hashedPassword;
+  return data;
+};
+
+UserSchema.methods.generateToken = function () {
+  const token = jwt.sign(
+    {
+      _id: this.id,
+      username: this.username,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '7d',
+    },
+  );
+  return token;
 };
 
 const User = mongoose.model('User', UserSchema);
