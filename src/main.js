39ca35960282a249979c6d572625cc8d4b391263
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-05-06 14:47:07 +0900

    Add koa-static to see files in frontend build directory

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 import api from './api';
 import jwtMiddleware from './lib/jwtMiddleware';
+import serve from "koa-static";
+import path from "path";
+import send from "koa-send";
 
 const { PORT, MONGO_URL } = process.env;
 
@@ -24,7 +27,16 @@ router.use('/api', api.routes());
 
 app.use(bodyParser());
 app.use(jwtMiddleware);
+
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../../blog-frontend/build');
+app.use(serve(buildDirectory));
+app.use(async ctx => {
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', {root: buildDirectory});
+  }
+})
 
 const port = PORT || 4000;
 
